80cccdb06f85bcdeeb1aa893d2641feb3dfb3310
Thomas Montet <montet.thomas@gmail.com>
2020-02-07 18:15:37 +0100

    search ok, select without multiple ok

--- src/js/App.js
+++ src/js/App.js
@@ -5,37 +5,34 @@ import '../scss/App.scss';
 import Dropdown from "./components/Dropdown.js";
 
 const options = [
-    {
-        text: "NoMeansNo",
-        value: "nomeansno"
-    },
-    {
-        text: "Boredoms",
-        value: "boredoms",
-        selected: true
-    },
-    {
-        text: "Boris",
-        value: "boris"
-    },
-    {
-        text: "Death Grips",
-        value: "death-grips",
-        selected: true
-    },
-    {
-        text: "Pissed Jeans",
-        value: "pissed-jeans"
-    }
+    {text: "NoMeansNo", value: "nomeansno"},
+    {text: "Boredoms", value: "boredoms"},
+    {text: "Mr. Bungle", value: "mr-bungle"},
+    {text: "Boris", value: "boris"},
+    {text: "Cardiacs", value: "cardiacs", selected: true},
+    {text: "Death Grips", value: "death-grips"},
+    {text: "Pissed Jeans", value: "pissed-jeans"}
+];
+
+const optionsMultiple = [
+    {text: "NoMeansNo", value: "nomeansno"},
+    {text: "Boredoms", value: "boredoms", selected: true},
+    {text: "Mr. Bungle", value: "mr-bungle"},
+    {text: "Boris", value: "boris"},
+    {text: "Cardiacs", value: "cardiacs"},
+    {text: "Death Grips", value: "death-grips", selected: true},
+    {text: "Pissed Jeans", value: "pissed-jeans"}
 ];
 
 const App = (props) => {
     return <div className="Container">
         <h1>Dropdown Component</h1>
         <h3>Default</h3>
-        <Dropdown options={options} multiple={true}/>
+        <Dropdown options={options}/>
         <h3>with title</h3>
-        <Dropdown title="Mes groupes préférés" options={options} multiple={true}/>
+        <Dropdown title="Mes groupes préférés" options={optionsMultiple} multiple={true}/>
+        <h3>with search</h3>
+        <Dropdown options={optionsMultiple} multiple={true} search={true}/>
     </div>
 }
 
--- src/js/components/Dropdown.js
+++ src/js/components/Dropdown.js
@@ -2,8 +2,9 @@ import React, { Component, useState, useEffect, useRef } from "react";
 import DropdownItem from "./dropdownItem.js";
 
 const Dropdown = (props) => {
-    const [selectedValues, setSelectedValues] = useState(props.options.filter(option => option.selected === true).map(({value}) => value));
-    const [buttonText, setButtonText] = useState(props.options[0].text);
+    const [options,setOptions] = useState(props.options);
+    const [selectedValues, setSelectedValues] = useState(options.filter(option => option.selected === true).map(({value}) => value));
+    const [buttonText, setButtonText] = useState("Sélectionnez une valeur");
     const [active,setActive] = useState(false);
     const dropdownRef = useRef(null);
 
@@ -13,44 +14,52 @@ const Dropdown = (props) => {
     const itemClickHandler = (e) => {
         let itemValue = e.target.getAttribute('data-value');
         let updatedSelectedValues;
-        if(selectedValues.includes(itemValue)){
-            updatedSelectedValues = selectedValues.filter(item => item != itemValue);
+        if(props.multiple){
+            if(selectedValues.includes(itemValue)){
+                updatedSelectedValues = selectedValues.filter(item => item != itemValue);
+            }else{
+                let unsortedValues = [...selectedValues,itemValue];
+                
+                //on repars de options pour conserver l'ordre d'affichage original des éléments
+                updatedSelectedValues = options.filter(item => unsortedValues.includes(item.value)).map(({value}) => value);
+            }
         }else{
-            let unsortedValues = [...selectedValues,itemValue];
-            
-            //on repars de props.options pour conserver l'ordre d'affichage original des éléments
-            updatedSelectedValues = props.options.filter(item => unsortedValues.includes(item.value)).map(({value}) => value);
+            updatedSelectedValues = [itemValue];
+            setButtonText(props.options.filter(option => option.value == itemValue)[0].text);
+            //si la sélection n'est pas multiple, on ferme le select au clic comme sur un select html classique
+            setActive(false);
         }
+        
         setSelectedValues(updatedSelectedValues);
     };
-    
+
+    //on filtre la recherche sur le texte, en lowercase
+    const filterValues = (e) => setOptions(props.options.filter(option => option.text.toLowerCase().includes(e.target.value)));
+
     useEffect(() => {
         documentClickHandler();
     }, []);
     
     return (
         <div className="Dropdown" ref={dropdownRef}>
-            {(() => {
-                if (props.title !== undefined) {
-                    return <span className="Dropdown-title">{props.title}</span>
-                }
-            })()}
+            {props.title !== undefined ? <span className="Dropdown-title">{props.title}</span> : false}
             <a className="Dropdown-button" onClick={buttonClickHandler} href="#">{buttonText}</a>
             <div className="Dropdown-list-container">
                 <ul className={active ? "Dropdown-list Dropdown-list--active" : "Dropdown-list"}>
-                {props.options.map((option,i) => {
-                    return <DropdownItem 
-                        key={i} 
-                        className={selectedValues.includes(option.value) ? "Dropdown-list-item Dropdown-list-item--selected" : "Dropdown-list-item"}
-                        value={option.value} 
-                        clickHandler={itemClickHandler}
-                        text={option.text}
-                    />
-                })}
+                    {props.search === true ? <li><input className="Dropdown-search" onChange={filterValues} type="text" placeholder="Search"/></li> : false}
+                    {options.map((option,i) => {
+                        return <DropdownItem 
+                            key={i} 
+                            className={selectedValues.includes(option.value) ? "Dropdown-list-item Dropdown-list-item--selected" : "Dropdown-list-item"}
+                            value={option.value} 
+                            clickHandler={itemClickHandler}
+                            text={option.text}
+                        />
+                    })}
                 </ul>
             </div>
-            <select defaultValue={selectedValues} multiple={props.multiple ? true : false}>
-            {props.options.map((option,i) => {
+            <select readOnly value={props.multiple ? selectedValues : selectedValues[0]} multiple={props.multiple ? true : false}>
+            {options.map((option,i) => {
                 return <option key={i} value={option.value}>{option.text}</option>
             })}
             </select>
