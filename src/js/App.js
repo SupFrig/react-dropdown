34db3123c9bc94e2a36f0b8efce5e30c573d58be
Tho Mon <montet.thomas@gmail.com>
2020-02-10 08:39:07 +0100

    wip offset + boutons filtre OK

--- src/js/App.js
+++ src/js/App.js
@@ -24,15 +24,37 @@ const optionsMultiple = [
     {text: "Pissed Jeans", value: "pissed-jeans"}
 ];
 
+const optionsVeryLong = [
+    {text: "NoMeansNo", value: "nomeansno"},
+    {text: "Boredoms", value: "boredoms", selected: true},
+    {text: "Mr. Bungle", value: "mr-bungle"},
+    {text: "Boris", value: "boris"},
+    {text: "Cardiacs", value: "cardiacs"},
+    {text: "Death Grips", value: "death-grips", selected: true},
+    {text: "Pissed Jeans", value: "pissed-jeans"},
+    {text: "Melt Banana", value: "melt-banana"},
+    {text: "Oceansize", value: "oceansize"},
+    {text: "Igorrr", value: "igorrr"},
+    {text: "Aphex Twin", value: "aphex-twin"},
+    {text: "Lightning Bolt", value: "lightning-bolt"},
+    {text: "Chai", value: "chai"},
+    {text: "Descendents", value: "descendents"},
+    {text: "The Frights", value: "the-frights"},
+    {text: "Misfits", value: "misfits"},
+    {text: "Wire", value: "wire"},
+];
+
 const App = (props) => {
     return <div className="Container">
         <h1>Dropdown Component</h1>
-        <h3>Default</h3>
+        <h3>Simple</h3>
         <Dropdown options={options}/>
-        <h3>with title</h3>
+        <h3>Multiple with title</h3>
         <Dropdown title="Mes groupes préférés" options={optionsMultiple} multiple={true}/>
-        <h3>with search</h3>
+        <h3>Multiple with search</h3>
         <Dropdown options={optionsMultiple} multiple={true} search={true}/>
+        <h3>Multiple with search, select & clear buttons, display offset</h3>
+        <Dropdown options={optionsVeryLong} multiple={true} search={true} clearButton={true} selectAllButton={true} offset={8}/>
     </div>
 }
 
--- src/js/components/Dropdown.js
+++ src/js/components/Dropdown.js
@@ -2,15 +2,31 @@ import React, { Component, useState, useEffect, useRef } from "react";
 import DropdownItem from "./dropdownItem.js";
 
 const Dropdown = (props) => {
-    const [options,setOptions] = useState(props.options);
-    const [selectedValues, setSelectedValues] = useState(options.filter(option => option.selected === true).map(({value}) => value));
-    const [buttonText, setButtonText] = useState("Sélectionnez une valeur");
-    const [active,setActive] = useState(false);
     const dropdownRef = useRef(null);
+    const defaultText = props.defaultText === undefined ? "Sélectionnez une valeur" : props.defaultText;
+    const useMountEffect = (fun) => useEffect(fun, [])
+    const [options,setOptions] = useState(props.options);
+    const [offset,setOffset] = useState(props.offset === undefined ? false : props.offset);
+    const [selectedValues, setSelectedValues] = useState(options.filter(option => option.selected === true).map(({value}) => value));
+    const [buttonText, setButtonText] = useState(defaultText);
+    const [active,setActive] = useState(false);
+    
 
     const buttonClickHandler = () => setActive(!active);
-    const documentClickHandler = () => document.addEventListener('click', (e) => dropdownRef.current.contains(e.target) ? false : setActive(false));
+    const documentClickHandler = () => {document.addEventListener('click', (e) => dropdownRef.current.contains(e.target) ? false : setActive(false))};
 
+    const defineText = () => {
+        if(selectedValues.length > 1){
+            //plusieurs options sélectionnées
+            setButtonText(`${selectedValues.length} valeurs sélectionnées`);
+        }else if(selectedValues.length == 1){
+            //une seule option sélectionnée
+            setButtonText(props.options.filter(option => option.value == selectedValues[0])[0].text);
+        }else{
+            //pas de sélection
+            setButtonText(defaultText);
+        }
+    };
     const itemClickHandler = (e) => {
         let itemValue = e.target.getAttribute('data-value');
         let updatedSelectedValues;
@@ -25,7 +41,6 @@ const Dropdown = (props) => {
             }
         }else{
             updatedSelectedValues = [itemValue];
-            setButtonText(props.options.filter(option => option.value == itemValue)[0].text);
             //si la sélection n'est pas multiple, on ferme le select au clic comme sur un select html classique
             setActive(false);
         }
@@ -36,17 +51,28 @@ const Dropdown = (props) => {
     //on filtre la recherche sur le texte, en lowercase
     const filterValues = (e) => setOptions(props.options.filter(option => option.text.toLowerCase().includes(e.target.value)));
 
+    //Mise à jour du texte quand la valeur est modifiée
+    useEffect(() => {
+        defineText();
+    }, [selectedValues]);
+
+    //exécution à la création du composant
     useEffect(() => {
         documentClickHandler();
+
+        if(offset) setOptions(options.slice(0,offset));
+        
     }, []);
-    
     return (
         <div className="Dropdown" ref={dropdownRef}>
             {props.title !== undefined ? <span className="Dropdown-title">{props.title}</span> : false}
             <a className="Dropdown-button" onClick={buttonClickHandler} href="#">{buttonText}</a>
             <div className="Dropdown-list-container">
                 <ul className={active ? "Dropdown-list Dropdown-list--active" : "Dropdown-list"}>
-                    {props.search === true ? <li><input className="Dropdown-search" onChange={filterValues} type="text" placeholder="Search"/></li> : false}
+                    {props.search ? <li><input className="Dropdown-search" onChange={filterValues} type="text" placeholder="Search"/></li> : false}
+                    {props.selectAllButton ? <li className="Dropdown-filter" onClick={() => setSelectedValues(options.map(({value}) => value))}>Select All</li> : false}
+                    {props.clearButton ? <li className="Dropdown-filter" onClick={() => setSelectedValues([])}>Clear All</li> : false}
+                    {props.selectAllButton || props.clearButton ? <li className="Dropdown-delimiter"></li> : false}
                     {options.map((option,i) => {
                         return <DropdownItem 
                             key={i} 
@@ -56,6 +82,7 @@ const Dropdown = (props) => {
                             text={option.text}
                         />
                     })}
+                    {(offset && offset < props.options.length) ? <li className="Dropdown-more">Déplier</li> : false}
                 </ul>
             </div>
             <select readOnly value={props.multiple ? selectedValues : selectedValues[0]} multiple={props.multiple ? true : false}>
